1e848953b872a5e72e7949f0e7828118693e987c
Craig McNamara <cmcnamara87@gmail.com>
2015-11-21 13:36:11 +1000

    feat(deploy): add manifest generator

--- gulpfile.js
+++ gulpfile.js
@@ -16,69 +16,81 @@ var karma = require('karma').server;
 /**
  * Test task, run test once and exit
  */
-gulp.task('test', function(done) {
+gulp.task('test', function (done) {
     karma.start({
         configFile: __dirname + '/karma.conf.js',
         //autoWatch: true,
         singleRun: true
-    }, function() {
+    }, function () {
         done();
     });
 });
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+    sass: ['./scss/**/*.scss']
 };
 
 gulp.task('default', ['sass']);
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+gulp.task('sass', function (done) {
+    gulp.src('./scss/ionic.app.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(gulp.dest('./www/css/'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest('./www/css/'))
+        .on('end', done);
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+gulp.task('watch', function () {
+    gulp.watch(paths.sass, ['sass']);
 });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
+gulp.task('install', ['git-check'], function () {
+    return bower.commands.install()
+        .on('log', function (data) {
+            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+        });
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+gulp.task('git-check', function (done) {
+    if (!sh.which('git')) {
+        console.log(
+            '  ' + gutil.colors.red('Git is not installed.'),
+            '\n  Git, the version control system, is required to download Ionic.',
+            '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+            '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+        );
+        process.exit(1);
+    }
+    done();
 });
 
 
-gulp.task('deploy', function() {
+gulp.task('deploy', ['manifest'], function () {
     return gulp.src('./www/**/*')
         .pipe(ghPages());
 });
 
-gulp.task('manifest', function() {
+gulp.task('manifest', function () {
     return gulp.src(['./www/**/*'])
         .pipe(calManifest({
-            load: ['lib/**/*.min.js', 'js/*.js', 'lib/**/*.css']
+            load: [
+                'js/cordova-app-loader-complete.js',
+                'js/autoupdate.js',
+                'lib/ionic/js/ionic.bundle.js',
+                'lib/lodash/lodash.js',
+                'js/app.js',
+                'js/controllers.js',
+                'js/services.js',
+                'js/scale.service.js',
+                'js/dash.controller.js',
+                'js/portions-factory.service.js',
+                'js/portions-prototype.service.js',
+                'js/bluetooth.service.js']
         }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./www/'));
 });
